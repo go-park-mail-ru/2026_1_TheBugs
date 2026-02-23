b43963300752e650199e48899fee870795540b18
Artemii <rerttdtdf@gmail.com>
2026-02-23 13:38:03 +0300

    базовые тесты для регистрации #4

---
+++ internal/usecase/auth/auth_test.go
@@ -0,0 +1,68 @@
+package auth
+
+import (
+	"testing"
+
+	"github.com/go-park-mail-ru/2026_1_TheBugs/internal/entity"
+	"github.com/go-park-mail-ru/2026_1_TheBugs/internal/entity/dto"
+	"github.com/go-park-mail-ru/2026_1_TheBugs/internal/repository"
+	"github.com/go-park-mail-ru/2026_1_TheBugs/internal/usecase"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+type MockUserRepo struct {
+	Slice []entity.User
+}
+
+func NewMockUserRepo(slice []entity.User) *MockUserRepo {
+	return &MockUserRepo{
+		Slice: slice,
+	}
+}
+
+func (r *MockUserRepo) GetUserByEmail(email string) (entity.User, error) {
+	for _, u := range r.Slice {
+		if u.Email == email {
+			return u, nil
+		}
+	}
+	return entity.User{}, repository.NotFoundRecord
+
+}
+
+func (r *MockUserRepo) CreateUser(dto dto.CreateUserDTO) (entity.User, error) {
+	id := uuid.New()
+	newUser := entity.User{
+		Id:             id.String(),
+		Email:          dto.Email,
+		HashedPassword: dto.HashedPassword,
+		Satl:           dto.Salt,
+	}
+	r.Slice = append(r.Slice, newUser)
+	return newUser, nil
+}
+
+func TestRegisterUseCaseOK(t *testing.T) {
+	testEmail := "email"
+	testPwd := "pwd"
+	mock := NewMockUserRepo(make([]entity.User, 0))
+
+	uc := NewAuthUseCase(mock)
+	err := uc.RegisterUseCase(testEmail, testPwd)
+	require.NoError(t, err)
+
+	u, err := mock.GetUserByEmail(testEmail)
+	require.NoError(t, err)
+
+	require.Equal(t, testEmail, u.Email)
+}
+
+func TestRegisterUseCaseErr(t *testing.T) {
+	testEmail := "email"
+	testPwd := "pwd"
+	mock := NewMockUserRepo([]entity.User{{Email: testEmail}})
+	uc := NewAuthUseCase(mock)
+	err := uc.RegisterUseCase(testEmail, testPwd)
+	require.ErrorIs(t, err, usecase.AlredyExitError)
+}
