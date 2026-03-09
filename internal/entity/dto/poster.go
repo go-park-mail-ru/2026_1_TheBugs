166c73c74ee0191a26fd951726d8f97d9cb1c71d
Artemiy <artemsikor18@gmail.com>
2026-03-09 18:51:43 +0300

    мок для кроватей (#12)

--- internal/entity/dto/poster.go
+++ internal/entity/dto/poster.go
@@ -20,7 +20,11 @@ type PostersFiltersDTO struct {
 
 func PostersToPostersDTO(posters []entity.Poster) []PosterDTO {
 	listPosters := make([]PosterDTO, 0, len(posters))
-	for _, poster := range posters {
+	var rating float64
+	var count int
+	for i, poster := range posters {
+		rating = float64((i/2)%10 + 1)
+		count = (i%5 + 1)
 		posterDTO := PosterDTO{
 			ID:      poster.Id,
 			Price:   poster.Price,
@@ -28,6 +32,8 @@ func PostersToPostersDTO(posters []entity.Poster) []PosterDTO {
 			Address: poster.Address,
 			Metro:   poster.Metro,
 			Area:    poster.Area,
+			Rating:  &rating,
+			Beds:    &count,
 		}
 
 		listPosters = append(listPosters, posterDTO)
