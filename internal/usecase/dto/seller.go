5f40df5f24ad81816aaa727523b4c6f9593d5c78
Artemiy <artemsikor18@gmail.com>
2026-04-01 16:25:02 +0300

    fix(seller): add make url

--- internal/usecase/dto/seller.go
+++ internal/usecase/dto/seller.go
@@ -1,6 +1,10 @@
 package dto
 
-import "github.com/go-park-mail-ru/2026_1_TheBugs/internal/entity"
+import (
+	"github.com/go-park-mail-ru/2026_1_TheBugs/config"
+	"github.com/go-park-mail-ru/2026_1_TheBugs/internal/entity"
+	"github.com/go-park-mail-ru/2026_1_TheBugs/internal/utils/photo"
+)
 
 type PosterSellerDTO struct {
 	SellerFirstName string  `json:"first_name"`
@@ -10,10 +14,16 @@ type PosterSellerDTO struct {
 }
 
 func posterToPosterSellerDTO(poster *entity.PosterById) PosterSellerDTO {
+	var url *string
+	if poster.SellerAvatarURL != nil {
+		avatar := photo.MakeUrlFromPath(*poster.SellerAvatarURL, config.Config.PublicHost, config.Config.Bucket)
+		url = &avatar
+	}
+
 	return PosterSellerDTO{
 		SellerFirstName: poster.SellerFirstName,
 		SellerLastName:  poster.SellerLastName,
 		SellerPhone:     poster.Phone,
-		SellerAvatarURL: poster.SellerAvatarURL,
+		SellerAvatarURL: url,
 	}
 }
